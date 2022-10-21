b3cdb8076d365d9d5dac97388918c2309a56c056
toughtosay <113202638+toughtosay@users.noreply.github.com>
2022-10-21 09:44:18 +0200

    Initial Commit

---
+++ CoreDataRelationsv1200/CoreDataRelationsv1200App.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataRelationsv1200App.swift
+//  CoreDataRelationsv1200
+//
+//  Created by Andree Carlsson on 2022-10-21.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataRelationsv1200App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
